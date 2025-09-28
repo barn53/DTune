e188e7e41858bebf15f3084da561f67fe06146b7
Gerd Müller <ge.mueller@sap.com>
2025-09-28 20:28:54 +0200

    action workflow

--- update_release_publish.sh
+++ update_release_publish.sh
@@ -6,6 +6,6 @@ git checkout main
 git rebase origin/main
 
 git checkout release
-git merge main -m "Merge branch 'main' into 'release'"
+git merge main -m "Merge branch 'main' into 'release' publish workflow"
 git push origin release
 
